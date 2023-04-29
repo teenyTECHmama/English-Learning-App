eef41729dc7c8410f5fcf5b9313a4933867bbd1f
teenyTECHmama <116871259+teenyTECHmama@users.noreply.github.com>
2023-04-29 17:55:28 +0200

    fixing buttons

---
+++ js/app.js
@@ -0,0 +1,11 @@
+var exitGame = []
+window._open = window.open;
+window._close = window.close;
+
+window.open = function(url,name,params) {
+    
+}
+
+window.close = function() {
+    while ( w = openedWindows.shift())
+}
