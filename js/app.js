6dd9c5e677c78cea3c39fda8729e36fe103d9a4e
teenyTECHmama <116871259+teenyTECHmama@users.noreply.github.com>
2023-07-25 16:25:46 +0200

    added audio

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var exitGame = []
+const exitGame = []
 window._open = window.open;
 window._close = window.close;
 
@@ -9,3 +9,21 @@ window.open = function(url,name,params) {
 window.close = function() {
     while ( w = openedWindows.shift());
 }
+
+
+
+ function jumpButton(button) {
+    button.style.animation = 'jump 0.6s ease';
+    setTimeout(() => {
+      button.style.animation = '';
+    }, 600);
+  }
+
+  // Add event listeners to the images to trigger the jump effect on their respective buttons
+  document.addEventListener('DOMContentLoaded', () => {
+    const buttons = document.querySelectorAll('.btn');
+    buttons.forEach(button => {
+      const img = button.closest('.secOne-item').querySelector('.secOne-item-img');
+      img.addEventListener('click', () => jumpButton(button));
+    });
+  });
